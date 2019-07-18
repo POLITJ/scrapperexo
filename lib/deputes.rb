da3b6bb5c6225c7bceb0bc325105d2e3af0086f7
POLITJ <jospolit@gmail.com>
2019-07-18 20:53:49 +0200

    final commit

--- lib/deputes.rb
+++ lib/deputes.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 	require 'nokogiri'
-​
+
 def get_url
   url = []
   doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/tableau"))
@@ -9,43 +9,49 @@ def get_url
   end
   url
 end
-​
+
 def get_emails
   i = -1
   emails = []
-    while i < get_url.size do
+  # Instead of 3 put get_url.size in order to get the full list of 577 deputies ;)  ==============================================
+    while i < 3 do
       i += 1
         doc = Nokogiri::HTML(open(get_url[i]))
           doc.xpath('//*[@id="haut-contenu-page"]/article/div[3]/div/dl/dd[4]/ul/li[2]/a').each do |node|
+            node.text
               emails << node.text
           end
     end
     emails
 end
-​
-def get_names
-  names = []
-  doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/tableau"))
-  doc.xpath('//td[1]/a').each do |node|
-    names << node.text.gsub("M. ", "").gsub("Mme ", "")
-  end
-  names
-end
-​
+
 def get_first_names
   first_names = []
   doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/tableau"))
   doc.xpath('//td[1]/a[1]').each do |node|
     first_names << node.text.gsub('M. ', '').gsub('Mme ', '').split.first
   end
-  puts first_names
+  first_names
 end
-​
+
 def get_last_names
   last_names = []
   doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/tableau"))
   doc.xpath('//td[1]/a[1]').each do |node|
     last_names << node.text.gsub('M. ', '').gsub('Mme ', '').split(' ')[1..-1].join(' ')
   end
-  puts last_names
+  last_names
 end
+
+def combine
+  test = []
+  i = 0
+  # In order to get the full list of 577 deputies replace 3 by get_first_names.size ==============================================
+    while i < 3 do
+      i += 1
+    test << {"First Name" => get_first_names[i], "Last Name" => get_last_names[i], "Email" => get_emails[i]}
+    end
+    test
+end
+
+print combine
--- lib/mairiechristmas.rb
+++ lib/mairiechristmas.rb
@@ -1,54 +1,55 @@
+
 require 'open-uri'
-	require 'nokogiri'
+require 'nokogiri'
 
 def get_url
-  url = []
-  doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-  doc.xpath('//tr/td/p//@href').each do |node|
-    url << node.text.gsub!('./', 'http://annuaire-des-mairies.com/')
-  end
-  url.drop(1)
+url = []
+doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+doc.xpath('//tr/td/p//@href').each do |node|
+url << node.text.gsub!('./', 'http://annuaire-des-mairies.com/')
+end
+url.drop(1)
 end
 
 def get_emails
-      emails = []
-      i = 0
-      while i < get_url.size do
-        i += 1
-    doc = Nokogiri::HTML(open(get_url[i]))
-    doc.xpath('//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
-      emails << node.text
-    end
-    end
-    emails
+  emails = []
+  i = -1
+  while i < get_url.size do
+    i += 1
+doc = Nokogiri::HTML(open(get_url[i]))
+doc.xpath('//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
+  emails << node.text
+end
+end
+emails
 end
 
 def get_city
-  city = []
-  doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-    doc.xpath("//tr/td/p/a").each do |node|
-      city << node.text
-    end
-      city
+city = []
+doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+doc.xpath("//tr/td/p/a").each do |node|
+  city << node.text
+end
+  city
 end
 
-def combine  ​
+def combine
 
-  myhash = Hash[get_city.zip get_emails]
+myhash = Hash[get_city.zip get_emails]
 
-  Hash.class_eval do
-    def split_into(get_emails.size)
-      count = 0
-      inject([]) do |final, key_value|
-        final[count%divisions] ||= {}
-        final[count%divisions].merge!({key_value[0] => key_value[1]})
-        count += 1
-        final
-      end
-    end
+Hash.class_eval do
+def split_into(get_emails.size)
+  count = 0
+  inject([]) do |final, key_value|
+    final[count%divisions] ||= {}
+    final[count%divisions].merge!({key_value[0] => key_value[1]})
+    count += 1
+    final
   end
-    a = [myhash.split_into(get_city.size)]
-    puts a
+end
+end
+a = [myhash.split_into(get_symbols.size)]
+puts a
 end
 
 combine
